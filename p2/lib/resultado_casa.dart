4d4ed1b479817f5281b5ae6b7f2e64aa0cbd01b2
AlexRP <aleeexrp02@gmail.com>
2024-04-26 16:19:05 +0200

    Cambio en pagina final

--- p2/lib/resultado_casa.dart
+++ p2/lib/resultado_casa.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
@@ -21,9 +20,6 @@ import 'modelo/CasaDeCampoBuilder.dart';
 import 'modelo/ChaletBuilder.dart';
 import 'modelo/CocinaConIsla.dart';
 
-
-
-
 class ItemListaFinal extends StatelessWidget {
   final String texto;
   final List<String> img;
@@ -38,15 +34,17 @@ class ItemListaFinal extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: [
-        Text(texto,style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+        Text(texto,
+            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
         SizedBox(height: 10),
         Column(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: List.generate(
             img.length,
-                (index) => Padding(
-              padding: EdgeInsets.only(bottom: 10), // Espacio vertical entre elementos
-              child: Image.network(
+            (index) => Padding(
+              padding: EdgeInsets.only(
+                  bottom: 10), // Espacio vertical entre elementos
+              child: Image.asset(
                 img[index],
                 width: 350,
                 height: 200,
@@ -55,7 +53,6 @@ class ItemListaFinal extends StatelessWidget {
             ),
           ),
         ),
-
         SizedBox(height: 20),
       ],
     );
@@ -66,15 +63,12 @@ class ResultadoCasa extends StatefulWidget {
   final CasaBuilder casaBuilder;
   String tipo = "";
 
-
-
   ResultadoCasa({super.key, required this.casaBuilder});
   @override
   _ResultadoCasaState createState() => _ResultadoCasaState();
 }
 
 class _ResultadoCasaState extends State<ResultadoCasa> {
-
   late DirectorCasa directorCasa;
   List<String> banioImg = [];
   List<String> cocinaImg = [];
@@ -92,50 +86,43 @@ class _ResultadoCasaState extends State<ResultadoCasa> {
     directorCasa = DirectorCasa(widget.casaBuilder);
     directorCasa.construirCasa();
 
-
     if (directorCasa.builder.runtimeType == ChaletBuilder) {
       widget.tipo = "Chalet";
       tipoDeCasaImg.add("assets/chalet.jpg");
-
     } else if (directorCasa.builder.runtimeType == CasaDeCampoBuilder) {
       widget.tipo = "Casa de Campo";
       tipoDeCasaImg.add("assets/campestre.jpg");
-
     } else if (directorCasa.builder.runtimeType == ApartamentoBuilder) {
       widget.tipo = "Apartamento";
       tipoDeCasaImg.add("assets/apartamento.jpg");
-
     } else {
       widget.tipo = "Error";
     }
 
-    for(int i=0;i<directorCasa.builder.casa.dormitorios.length;i++){
-      especificacionDormitorios += "Dormitorio ${i+1}: ${directorCasa.builder.casa.dormitorios[i].dormitorio}\n";
+    for (int i = 0; i < directorCasa.builder.casa.dormitorios.length; i++) {
+      especificacionDormitorios +=
+          "Dormitorio ${i + 1}: ${directorCasa.builder.casa.dormitorios[i].dormitorio}\n";
     }
 
-
-    if(directorCasa.builder.casa.dormitorios.length>1){
+    if (directorCasa.builder.casa.dormitorios.length > 1) {
       dormitoriosImg.add("assets/dormitorioNiños.jpeg");
     }
     dormitoriosImg.add("assets/dormitorioMatrimonio.jpeg");
 
-
-
-
     salaDeEstarImg.add("assets/salaEstar.jpeg");
-    
+
     cocinaImg.add("assets/cocina.jpg");
 
     cocinaAsignada = directorCasa.builder.casa.cocina.toString();
-    List<String>partes = cocinaAsignada.split(" ");
+    List<String> partes = cocinaAsignada.split(" ");
 
     cocinaAsignada = "";
 
-    for(var i=0;i<partes.length;i++){
-      if(partes[i]=="lavavajillas"){
+    for (var i = 0; i < partes.length; i++) {
+      if (partes[i] == "lavavajillas") {
         cocinaImg.add("assets/lavavajillas.jpg");
         cocinaAsignada += " Con Lavavajillas";
-      }else if(partes[i]=="isla"){
+      } else if (partes[i] == "isla") {
         cocinaImg.add("assets/isla.jpg");
         cocinaAsignada += " Con Isla";
       }
@@ -148,17 +135,15 @@ class _ResultadoCasaState extends State<ResultadoCasa> {
 
     banioAsignado = "";
 
-    for(var i=0;i<partes.length;i++){
-      if(partes[i]=="bidet"){
+    for (var i = 0; i < partes.length; i++) {
+      if (partes[i] == "bidet") {
         banioImg.add("assets/bidet.jpg");
         banioAsignado += " Con Bidet";
-      }else if(partes[i]=="jacuzzi"){
+      } else if (partes[i] == "jacuzzi") {
         banioImg.add("assets/jacuzzi.webp");
         banioAsignado += " Con Jacuzzi";
       }
     }
-
-
   }
 
   @override
@@ -177,50 +162,56 @@ class _ResultadoCasaState extends State<ResultadoCasa> {
               ),
             ),
             SizedBox(height: 20),
-            Container(
-              height: 530, // Altura deseada de la lista
+            Expanded(
               child: ListView(
                 scrollDirection: Axis.vertical, // Cambiar a vertical
                 children: [
                   SizedBox(height: 20), // Espacio arriba
                   ItemListaFinal(texto: widget.tipo, img: tipoDeCasaImg),
                   SizedBox(height: 20), // Espacio entre elementos
-                  ItemListaFinal(texto: "Tienes ${directorCasa.builder.casa.dormitorios.length} dormitorios \n" + especificacionDormitorios, img: dormitoriosImg),
+                  ItemListaFinal(
+                      texto:
+                          "Tienes ${directorCasa.builder.casa.dormitorios.length} dormitorios \n" +
+                              especificacionDormitorios,
+                      img: dormitoriosImg),
                   SizedBox(height: 20), // Espacio entre elementos
-                  ItemListaFinal(texto: directorCasa.builder.casa.salaDeEstar.toString(), img: salaDeEstarImg),
+                  ItemListaFinal(
+                      texto: directorCasa.builder.casa.salaDeEstar.toString(),
+                      img: salaDeEstarImg),
                   SizedBox(height: 20), // Espacio entre elementos
-                  ItemListaFinal(texto: "Cocina"+cocinaAsignada, img: cocinaImg),
+                  ItemListaFinal(
+                      texto: "Cocina" + cocinaAsignada, img: cocinaImg),
                   SizedBox(height: 20), // Espacio entre elementos
-                  ItemListaFinal(texto: "Baño"+banioAsignado, img: banioImg),
+                  ItemListaFinal(texto: "Baño" + banioAsignado, img: banioImg),
                   SizedBox(height: 20), // Espacio abajo
+                  Padding(
+                    padding: const EdgeInsets.only(top: 15),
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        backgroundColor: Colors.white,
+                        shadowColor: Colors.white,
+                        surfaceTintColor: Colors.white,
+                        foregroundColor: Colors.black,
+                        side: const BorderSide(color: Colors.black, width: 3),
+                      ),
+                      onPressed: () => Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => PantallaFinal(),
+                        ),
+                      ),
+                      child: const Text(
+                        "Continuar",
+                        style: TextStyle(
+                          fontSize: 40,
+                        ),
+                      ),
+                    ),
+                  ),
                 ],
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(top: 15),
-              child: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  backgroundColor: Colors.white,
-                  shadowColor: Colors.white,
-                  surfaceTintColor: Colors.white,
-                  foregroundColor: Colors.black,
-                  side: const BorderSide(color: Colors.black, width: 3),
-                ),
-                onPressed: () => Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => PantallaFinal(),
-                  ),
-                ),
-                child: const Text(
-                  "Continuar",
-                  style: TextStyle(
-                    fontSize: 40,
-                  ),
-                ),
               ),
             ),
           ],
